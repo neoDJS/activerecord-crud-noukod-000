4607c407ec1e857fab919b5837b5821acae293dc
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-17 18:58:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,8 +92,9 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once").tap do |m|
+    m.delete
+  end
 end
 
 def can_destroy_all_items_at_once
